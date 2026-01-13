68c46b3a0792461252efa5800ede0e7e7a335184
“abhi-abhi86” <abhishekmgabhishekmg829@gmail.com>
2026-01-13 08:35:44 +0530

    refactor: update social links, templates and models

--- my_portfolio/apps/updates/models.py
+++ my_portfolio/apps/updates/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Update(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='updates/', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
